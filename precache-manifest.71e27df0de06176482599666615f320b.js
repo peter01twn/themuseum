54051523c8d2e0223b66d872347e20e4050f5684
peter <peter01twn@gmail.com>
2020-04-24 18:53:13 +0800

    Updates

---
+++ precache-manifest.71e27df0de06176482599666615f320b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9fc788906762d9077c548bee1d30136",
+    "url": "/The-Museum/index.html"
+  },
+  {
+    "revision": "dada444b7f266566138f",
+    "url": "/The-Museum/static/css/main.36f99c32.chunk.css"
+  },
+  {
+    "revision": "589fc64df5837910512f",
+    "url": "/The-Museum/static/js/2.f0f25184.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/The-Museum/static/js/2.f0f25184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dada444b7f266566138f",
+    "url": "/The-Museum/static/js/main.9d7652ec.chunk.js"
+  },
+  {
+    "revision": "5b73b7363117f0fc6b15",
+    "url": "/The-Museum/static/js/runtime-main.9a54f36d.js"
+  }
+]);
